b69f6a4b7ab3f6b5a82e2517a4027b7218d0e39e
6ixfalls <23470032+6ixfalls@users.noreply.github.com>
2023-04-21 16:55:09 -0700

    try to fix routes

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,10 @@ nextApp.prepare().then(() => {
         app.use(router);
     }
 
-    app.get("/admin*", (req, res) => {
+    app.get("/admin", (req, res) => {
+        return nextHandler(req, res);
+    });
+    app.get("/admin/*", (req, res) => {
         return nextHandler(req, res);
     });
 
